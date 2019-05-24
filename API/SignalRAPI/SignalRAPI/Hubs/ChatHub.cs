3e4c05a3fe4b95dc5e3c76143830b875242cb07c
CHILLI\Mahesh <Mahesh.Moodley@chillisoft.co.za>
2019-05-24 16:31:59 +0200

    feat: upgrade api to .net core 2.2

---
+++ API/SignalRAPI/SignalRAPI/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using SignalRAPI.Models;
+
+namespace SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(ChatMessage message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
---
+++ API/SignalRAPI/SignalRAPI/Models/ChatMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRAPI.Models
+{
+    public class ChatMessage
+    {
+        public string user { get; set; }
+        public string message { get; set; }
+        public string room { get; set; }
+    }
+}
